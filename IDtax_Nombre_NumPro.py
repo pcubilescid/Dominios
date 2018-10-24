83fe0accb623527697fc9d334ddf84e0cfc07fa0
Pacuci <pcubilescid@gmail.com>
2018-10-24 09:46:55 +0200

    nombre del organismo

--- IDtax_Nombre_NumPro.py
+++ IDtax_Nombre_NumPro.py
@@ -6,7 +6,12 @@ def readUniProt(input):
         for line in f:
             line = line.decode()  # pasamos de line typo bit a tipo string
             if line[0:2] == 'OX':
-                linea = re.sub("\D","",line)
-                print(linea)
+                ID = re.sub("\D","",line)
+                print(ID)
+            if line[0:2] == 'OS':
+                line = line.replace("OS ","")
+                nombre = line[2:-2]
+
+                print(nombre)
 
 readUniProt(sys.argv[1])
