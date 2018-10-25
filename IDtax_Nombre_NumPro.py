db667942d21aeec29a34fe87c31b0c24c9ceb2f2
Pacuci <pcubilescid@gmail.com>
2018-10-25 12:21:16 +0200

    Resultados tabulate

--- IDtax_Nombre_NumPro.py
+++ IDtax_Nombre_NumPro.py
@@ -1,17 +1,37 @@
 import sys
 import re
 import gzip
+from tabulate import tabulate
 def readUniProt(input):
     with gzip.open(input, 'r') as f: #abrimos fichero fasta, solo lectura
+        i = 0
+        final = []
+        resultado = {}
+        nombre = ''
+        pro = 1
+        repite = 0
+
         for line in f:
             line = line.decode()  # pasamos de line typo bit a tipo string
             if line[0:2] == 'OX':
-                ID = re.sub("\D","",line)
-                print(ID)
-            if line[0:2] == 'OS':
-                line = line.replace("OS ","")
-                nombre = line[2:-2]
+                repite = 0
+                line = line.split('{')
+                id = re.sub("\D","",line[0])
+                if id in resultado:
+                    pro = resultado[id][2]
+                    pro += 1
+                    resultado[id][2] = pro
+                else:
+                    resultado[id] = [id,nombre,pro]
 
-                print(nombre)
+            if line[0:2] == 'OS':
+                line = line.replace("OS   ", "")
+                line = line.replace(".", "")
+                if repite == 1:
+                    nombre += line[:-1]
+                else:
+                    nombre = line[:-1]
+                    repite = 1
+        print(tabulate(resultado.values()))
 
 readUniProt(sys.argv[1])
