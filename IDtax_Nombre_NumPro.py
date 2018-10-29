63be0984b5d8fa0d9b320498a88765dea4d20d8e
Pacuci <pcubilescid@gmail.com>
2018-10-29 10:03:47 +0100

    Comentar codigo IDtax_Nombre_NumPro.py

--- IDtax_Nombre_NumPro.py
+++ IDtax_Nombre_NumPro.py
@@ -3,37 +3,33 @@ import re
 import gzip
 from tabulate import tabulate
 def readUniProt(input,output):
-    with gzip.open(input, 'r') as f: #abrimos fichero fasta, solo lectura
-        i = 0
-        resultado = {}
-        nombre = ''
+    with gzip.open(input, 'r') as f:  # abrimos fichero input, solo lectura
+        resultado = {} # declaramos resultado como diccinario
         pro = 1
         repite = 0
 
-        for line in f:
+        for line in f :# para line dentro de fichero
             line = line.decode()  # pasamos de line typo bit a tipo string
             resultado[0]=["TaxID","Scientific Name (Common Name)","Num_Proteins"]
-            if line[0:2] == 'OX':
-                repite = 0
-                line = line.split('{')
-                id = re.sub("\D","",line[0])
-                if id in resultado:
-                    resultado[id][2] += pro
-                else:
-                    resultado[id] = [id,nombre,pro]
+            if line[0:2] == 'OX': # si la line entre la posicion 0 y 2 es igual a OX
+                repite = 0 # repite igual a 0
+                line = line.split('{') # separamos line cuando encuentre el caracter {
+                id = re.sub("\D","",line[0]) # id es igual a todos los numeros que esten en line antes del caracter {, es decir en line de la posicion 0 de line
+                if id in resultado: # si id esta dentro de resultado
+                    resultado[id][2] += pro # resultado en la posicion id, en la posicion 2 se suma pro(1)
+                else: # sino
+                    resultado[id] = [id,nombre,pro] #resultado en la posicion id sera igual a posicion 0 id, posicion 1 nombre, posicion 2 pro
 
-            if line[0:2] == 'OS':
-                line = line.replace("OS   ", "")
-                line = line.replace(".", "")
-                if repite == 1:
-                    nombre += line[:-1]
-                else:
-                    nombre = line[:-1]
-                    repite = 1
+            if line[0:2] == 'OS': # si line entre la posicion 0 y 2 es igual a OS
+                line = line.replace("OS   ", "") # line es igual a line camiando 'OS   ' a ''
+                line = line.replace(".", "") # line es igual a line cambiando '.' ''
+                if repite == 1: # si repite es igual a 1
+                    nombre += line[:-1] # nombre es igual al contenido que ya tenia nombre, mas line quitandole la ultima posicion (\n)
+                else: # sino
+                    nombre = line[:-1] # nombre es igual a line menos la ultima posicion (\n), sobreescribe lo que tenia antes nombre
+                    repite = 1 # repite es igual a 1
 
-        print(tabulate(resultado.values()))
-
-    with open(output, 'w') as w:
-        w.write(tabulate(resultado.values(),headers="firstrow"))
+    with open(output, 'w') as w: # abrimos fichero output, solo escritura
+        w.write(tabulate(resultado.values(),headers="firstrow"))# escribimos en el fichero, en formato tabular, los valores de resultado y poniendo la primera fila como encabezado
 
 readUniProt(sys.argv[1],sys.argv[2])
