919a3710b6330a6be8252633f359018cc152903a
Pacuci <pcubilescid@gmail.com>
2018-12-12 09:37:03 +0100

    Dominios Uniprot funciona -codigo guarro-

--- EJERCICIO7/Ejercicio7.py
+++ EJERCICIO7/Ejercicio7.py
@@ -1,15 +1,66 @@
 import sys
 from tabulate import tabulate
+import gzip
 
 def Ejercicio7(fasta,uniprot,tabular,output):
     resultado = {} # declaramos resultado como diccinario
     id_proteina = {}
     id_tax = {}
     id_hmm = {}
+    id_uni = {}
+    pfam = {}
     ac = {}
     ac2 = {}
+    ac_number = {}
 
     resultado[0] = ["ID TAX", "ID PROTEINA", "ID DOMINIOS", "ID DOMINIOS UNIPROT", "ID DOMINIOS HMMER"]
+
+    with gzip.open(uniprot, 'r') as u:  # abrimos fichero input, solo lectura
+        i = 1
+        j = 1
+        repite = 0
+        for line3 in u:  # para line dentro de fichero
+            line3 = line3.decode()  # pasamos de line typo bit a tipo string
+            if line3[0:2] == 'AC':  # si la line entre la posicion 0 y 2 es igual a AC
+                line3 = line3.replace("AC   ", "")  # line es igual a line camiando 'AC   ' por ''
+                line3 = line3.replace(";", "|")  # line es igual a line cambiando ';' por '|'
+                line3 = line3.replace(" ", "")  # line es igual a line cambiando ' ' por ''
+                ac_number[j] = line3[0:-2]  # ac_number es igual a line menos las 2 ultimas posiciones
+                j += 1
+                i += 1
+                repite = 0
+
+            if line3[:10] == 'DR   Pfam;':
+                line3 = line3.replace(" ", "")  # line es igual a line cambiando ' ' por ''
+                line3 = line3.split(';')  # separamos line cuando encuentre el caracter ;
+                if repite == 0:
+                    pfam[i] = line3[1]  # id_hmm[i] es igual a line2 en la posicion uno
+                    repite = 1
+                else:
+                    pfam[i] += "," + line3[1]
+
+    with open(fasta, 'r') as f:  # abrimos fichero input, solo lectura
+        i = 1
+        j = 1
+
+        for line in f:  # para line dentro de fichero
+            pfam[351]="-"
+            pfam[2404] = "-"
+           # try:
+            if line[0] == '>':  # si la line entre la posicion 0 y 2 es igual a OX
+                line = line.split(';')  # separamos line cuando encuentre el caracter ;
+                ac[j] = line[0][1:] # ac[i] es igual a line de la posicion 0 desde el caracter 1 hasta el final
+                while ac_number[i] != ac[j]: # si ac2[j] es igual a ac[i] (ac son accesion number)
+                    i += 1
+                if ac_number[i] == ac[j]:
+                    print(pfam[i])
+                    id_uni[j] = pfam[i]
+                    j += 1
+
+            #except KeyError:
+                #id_uni[j] = "-"
+               # j += 1
+
     with open(fasta, 'r') as f:  # abrimos fichero input, solo lectura
         i = 1
         for line in f:  # para line dentro de fichero
@@ -27,26 +78,27 @@ def Ejercicio7(fasta,uniprot,tabular,output):
         for line2 in t: # para line2 dentro de fichero t
             line2 = line2.split()  # separamos line cuando encuentre un espacio
             id_hmm[1]='-'
+            dom = line2[1].split(".")
             if line2[1][0] == "P": # si line2 en la posicion 1 el caracter 0 es igual a P
                 ac2[j] = line2[0] # ac2[j] es igual a line2 posicion 0
                 if ac2[j] == ac[i]: # si ac2[j] es igual a ac[i] (ac son accesion number)
                     if repite == 1: # si repite es igual  a 1
-                        id_hmm[i] += "  " + line2[1] # id_hmm[i] es mas igual a espacio mas line2 en la posicion 1 (concatetar los dominios)
+                        id_hmm[i] += "," + dom[0] # id_hmm[i] es mas igual a espacio mas line2 en la posicion 1 (concatetar los dominios)
                     else:
-                        id_hmm[i] = line2[1] # id_hmm[i] es igual a line2 en la posicion uno
+                        id_hmm[i] = dom[0] # id_hmm[i] es igual a line2 en la posicion uno
                         repite = 1
                     j += 1 # añadimos 1 al contador j
                 else:
                     repite=0
                     i += 1 # añadimos 1 al contador i
                     if ac2[j] == ac[i]: # si ac2[j] es igual a ac[i] (ac son accesion number)
-                        id_hmm[i] = line2[1] # id_hmm[i] es igual a line2 en la posicion uno
+                        id_hmm[i] = dom[0] # id_hmm[i] es igual a line2 en la posicion uno
                         repite = 1
                         j += 1 # añadimos 1 al contador j
 
     i = 1
     for x in id_proteina: # para x dentro de id_proteina
-        resultado[i] = [id_tax[i], id_proteina[i], "-", "-", id_hmm[i]]
+        resultado[i] = [id_tax[i], id_proteina[i], "-", id_uni[i], id_hmm[i]]
         i += 1
 
 
